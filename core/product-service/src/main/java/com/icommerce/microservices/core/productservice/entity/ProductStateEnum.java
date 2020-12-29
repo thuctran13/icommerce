79fc578dde81264928cc230689de9a70e8a6d1b0
Thuc Tran <thuc.tran@axonactive.com>
2020-12-29 08:14:37 -0800

    M1: add persistence unit - create entity - add h2 config

---
+++ core/product-service/src/main/java/com/icommerce/microservices/core/productservice/entity/ProductStateEnum.java
@@ -0,0 +1,5 @@
+package com.icommerce.microservices.core.productservice.entity;
+
+public enum ProductStateEnum {
+    CREATED, PUBLISHED, DELETED 
+}
--- core/product-service/src/main/java/com/icommerce/microservices/core/productservice/service/ProductService.java
+++ core/product-service/src/main/java/com/icommerce/microservices/core/productservice/service/ProductService.java
@@ -1,12 +1,22 @@
 package com.icommerce.microservices.core.productservice.service;
 
-import com.icommerce.microservices.core.productservice.model.Product;
+import com.icommerce.microservices.core.productservice.dao.ProductDAO;
+import com.icommerce.microservices.core.productservice.dto.ProductInfo;
+import com.icommerce.microservices.core.productservice.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class ProductService {
+    
+    @Autowired
+    private ProductDAO productDAO;
 
     /**
      * Sample usage: curl $HOST:$PORT/product/1
@@ -14,9 +24,38 @@ public class ProductService {
      * @param productId
      * @return
      */
-    @RequestMapping("/product/{productId}")
-    public Product getProduct(@PathVariable int productId) {
-
-        return new Product(productId, "name", 123);
+    @RequestMapping(value = "/product/{productId}", method = RequestMethod.GET)
+    public ProductInfo get(@PathVariable Long productId) {
+        Product product = productDAO.findById(productId).orElse(null);
+        return new ProductInfo(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getState(),
+                product.getStatus(),
+                product.getPrice(),
+                product.getWeight(),
+                product.getCreationTs(),
+                product.getModifyTs()
+        );
     }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public ProductInfo create(@RequestBody() ProductInfo productInfo) {
+        Product product = new Product();
+        product.setName(productInfo.getName());
+        product.setDescription(productInfo.getDescription());
+        product.setState(productInfo.getState());
+        product.setStatus(productInfo.getStatus());
+        product.setPrice(productInfo.getPrice());
+        product.setWeight(productInfo.getWeight());
+        product.setCreationTs(new Date());
+        product.setCreationUid("admin");
+        product.setModifyTs(new Date());
+        product.setModifyUid("admin");
+        product.setCtn(0L);
+        productDAO.save(product);
+        return productInfo;
+    }
+    
 }
