144e605d8ab1b492eda488fa54bfa44edc3845f3
Thuc Tran <thuc.tran@axonactive.com>
2020-12-29 07:04:47 -0800

    M1: initial services - add support service

---
+++ support/discovery-server/src/test/java/com/icommerce/microservices/support/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icommerce.microservices.support.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ support/edge-server/src/test/java/com/icommerce/microservices/support/edgeserver/EdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icommerce.microservices.support.edgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
