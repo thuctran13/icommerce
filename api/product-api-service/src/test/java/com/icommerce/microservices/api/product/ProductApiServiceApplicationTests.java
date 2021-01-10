6964a54625de559b0d2c1cd2cc097be64c77f608
Thuc Tran <thuc.tran@axonactive.com>
2021-01-10 06:15:59 -0800

    M3: Init auth service

---
+++ api/product-api-service/src/test/java/com/icommerce/microservices/api/product/ProductApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icommerce.microservices.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ support/auth-server/src/main/java/com/icommerce/microservices/support/authserver/AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.icommerce.microservices.support.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
